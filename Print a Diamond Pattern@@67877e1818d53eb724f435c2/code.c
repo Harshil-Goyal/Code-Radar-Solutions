387846b010f5383d567fe0667f55847b1b3eda9b
Harshil Goyal <goyalharshil649@gmail.com>
2025-03-11 12:26:34 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -5,12 +5,16 @@ int main()
     int i, j, a;
     scanf("%d", &a);
 
+    // Loop through the rows
     for(i = 1; i <= a; i++)  
     {
+        // Calculate the number of stars in the current row
         int k = (i <= a / 2) ? i : a - i + 1;
 
+        // Loop through columns for each row
         for(j = 1; j <= a; j++)  
         {
+            // Print star if we are within the range for the current row
             if(j >= (a / 2) - k + 1 && j <= (a / 2) + k)  
                 printf("*");
             else
